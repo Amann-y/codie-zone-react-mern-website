cea2d4e35fd7e320cf08319ed1b1e9b69dec41ee
Amann-y <yadavaman413@gmail.com>
2023-12-26 15:06:31 +0530

    Only admin can access users and contacts

---
+++ server/middlewares/admin-middleware.js
@@ -0,0 +1,14 @@
+
+const adminMiddleware = async(req,res,next)=>{
+    try {
+        const adminRole = req.user.isAdmin
+        if(!adminRole){
+            return res.status(403).json({message:"Access Denied, User Is Not An Admin"})
+        }
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = adminMiddleware
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -4,10 +4,11 @@ const {
   getAllContacts,
 } = require("../controllers/admin-controller");
 const authMiddleware = require("../middlewares/auth-middleware");
+const adminMiddleware = require("../middlewares/admin-middleware");
 
 const router = express.Router();
 
-router.get("/users", authMiddleware, getAllUsers);
-router.get("/contacts", authMiddleware, getAllContacts);
+router.get("/users", authMiddleware,adminMiddleware, getAllUsers);
+router.get("/contacts", authMiddleware,adminMiddleware, getAllContacts);
 
 module.exports = router;
